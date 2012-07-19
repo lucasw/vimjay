779e448812998dba2ce0b5906d01a26e236c23d1
Lucas Walter <wsacul@gmail.com>
2012-07-19 05:46:45 +0000

    buffer is working now
    git-svn-id: http://binarymillenium.googlecode.com/svn/trunk@656 3bc78312-1b2a-0410-bf3d-39cd8132870d

--- camthing.cpp
+++ camthing.cpp
@@ -14,6 +14,48 @@
 using namespace cv;
 using namespace std;
 
+class Buffer
+{
+
+  deque<cv::Mat> frames;
+  int max_size;
+  
+  public:
+  Buffer(const int max_size=180) {
+    this->max_size = max_size;
+    LOG(INFO) << "new buffer max_size " << this->max_size;
+  }
+
+  void add(cv::Mat new_frame)
+  {
+    frames.push_back(new_frame);
+
+    while (frames.size() >= max_size) frames.pop_front();
+  }
+
+  cv::Mat get(const float fr) 
+  {
+    if (frames.size() < 1) {
+      VLOG(1) << "no frames returning gray";
+      cv::Mat tmp = cv::Mat(640,480,CV_8UC3);
+      tmp = cv::Scalar(128);
+      return tmp;
+    }
+    int ind = (int)(fr*(float)frames.size());
+    if (fr < 0) {
+      ind = frames.size() - ind;
+    }
+    
+    ind %= frames.size();
+   
+    //VLOG_EVERY_N(1,10) 
+    LOG_EVERY_N(INFO,10) << ind << " " << frames.size();
+    return frames[ind];
+  }
+
+
+};
+
 /*
  * To work with Kinect the user must install OpenNI library and PrimeSensorModule for OpenNI and
  * configure OpenCV with WITH_OPENNI flag is ON (using CMake).
@@ -24,6 +66,7 @@ TBD have a mode that takes a webcam, uses brightness as depth, and thresholds it
 int main( int argc, char* argv[] )
 {
   google::InitGoogleLogging(argv[0]);
+  google::LogToStderr();
   // pair of rgb images and depths put together
 
   LOG(INFO) << "camera opening ...";
@@ -37,15 +80,18 @@ int main( int argc, char* argv[] )
     LOG(ERROR) << "Can not open a capture object.";
     return -1;
   }
-    
 
-  // Print some avalible Kinect settings.
-  LOG(INFO) << "\nDepth generator output mode:" << endl <<
-    "FRAME_WIDTH    " << capture.get( CV_CAP_PROP_FRAME_WIDTH ) << endl <<
-    "FRAME_HEIGHT   " << capture.get( CV_CAP_PROP_FRAME_HEIGHT ) << endl <<
-    "FPS    " << capture.get( CV_CAP_PROP_FPS ) << endl;
+  Buffer* cam_buf = new Buffer();
 
-  cv::namedWindow("frame", CV_GUI_NORMAL);
+  cv::namedWindow("cam", CV_GUI_NORMAL);
+  cv::moveWindow("cam",0,0);
+  cv::namedWindow("out", CV_GUI_NORMAL);
+  cv::moveWindow("out",640,0);
+ 
+  // get the first black frames out
+  capture.grab();
+  capture.grab();
+
   for(;;)
   {
 
@@ -63,7 +109,10 @@ int main( int argc, char* argv[] )
         cout << "bad capture" << endl;
         continue;
       }
-      imshow("frame",frame);
+      imshow("cam",frame);
+      // I think opencv is reusing a mat within capture so have to clone it
+      cam_buf->add(frame.clone());
+      imshow("out",cam_buf->get(0.0));
     }
 
     if( waitKey( 1 ) == 'q' )
