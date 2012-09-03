7ec669f030270b98888df4d6cb0c51d423a1616d
Lucas Walter <wsacul@gmail.com>
2012-09-03 04:45:28 +0000

    Place holder for mouse input- multiple mice is a must!
    git-svn-id: http://binarymillenium.googlecode.com/svn/trunk@830 3bc78312-1b2a-0410-bf3d-39cd8132870d

---
+++ input.h
@@ -0,0 +1,31 @@
+#ifndef __SIGNALS_H__
+#define __SIGNALS_H__
+
+#include <iostream>
+#include <stdio.h>
+
+#include <boost/thread.hpp>
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+//#include <random>
+#include <deque>
+#include <map>
+#include "nodes.h"
+
+namespace bm {
+
+// input devices will present all as a set of Signals, for instance button clicks will be 0 or 1
+// and analog output will be 0-32768 or for mice the resolution of the screen
+
+// Mouse - support multiple mice
+//  also output dx, dy, mousewheel
+
+// Joystick
+// https://github.com/Grumbel/jstest-gtk/blob/master/src/joystick.cpp
+
+// Future -- wiimote?
+
+} // bm
+#endif // MISC_NODES
