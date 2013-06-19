923ec7649b73b6e3d270cab126bb97ba8104e131
Lucas Walter <wsacul@gmail.com>
2013-06-19 06:29:49 -0700

    Need to use setOut to have index control of buffer output.  TBD need to make sure cur_size gets updated by adding frames, currently isn't.

--- video.cpp
+++ video.cpp
@@ -60,8 +60,8 @@ namespace bm {
   {
     is_thread_dirty = false;
     setSignal("mode", 0, false, ROLL, 0, 4);
-    //setString("file", "../data/test.mp4");
-    setString("file", "../data/test.webm");
+    setString("file", "../data/test.mp4");
+    //setString("file", "../data/test.webm");
     
     LOG(INFO) << name << " video";
     cam_thread = boost::thread(&Video::runThread, this);
@@ -147,7 +147,7 @@ namespace bm {
     if (!rv) return false;
    
     // set the out to whatever the ind input is asking for
-    manualUpdate();
+    setOut();
 
     return true;
   }
