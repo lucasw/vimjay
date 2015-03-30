ffd8968d77d492ac0c4a0cb579ff0b4fc7df71fc
Lucas Walter <wsacul@gmail.com>
2015-03-29 20:58:52 -0700

    Now all the classes are templated so they can use ceres jet, though haven't tested that yet

--- src/nngen/nn_im.cpp
+++ src/nngen/nn_im.cpp
@@ -34,12 +34,13 @@ from the second layer scales the basis image coefficients of the first in other 
 
 // NodeDiff class that has pointers to two nodes
 // and reports the cost as the difference between them.
+template <typename T>
 struct NodeDiffFunctor
 {
   NodeDiffFunctor(
-      Net* net,
-      Node* input,
-      Node* output
+      Net<T>* net,
+      Node<T>* input,
+      Node<T>* output
       ) :
       input_(input),
       output_(output)
@@ -47,7 +48,6 @@ struct NodeDiffFunctor
   }
 
   // the input is actually every weight in the entire network
-  template <typename T>
   bool operator() (const T* const x, T* residual) const 
   {
     // iterate through all the weights and assign them values out of T- 
@@ -59,19 +59,20 @@ struct NodeDiffFunctor
     return true;
   }
 
-  const Node* const input_;
-  const Node* const output_;
+  const Node<T>* const input_;
+  const Node<T>* const output_;
 };
 
 
-
-Weight::Weight(std::string name) : 
-    Base(name) 
+template <typename T>
+Weight<T>::Weight(std::string name) : 
+    Base<T>(name) 
 {
 
 }
 
-Node::Node(
+template <typename T>
+Node<T>::Node(
     const std::string name, 
     const int x, const int y, const int z,
     const bool use_var, const bool use_mean, const bool use_tanh) :
@@ -81,54 +82,58 @@ Node::Node(
     use_var_(use_var),
     use_mean_(use_mean),
     use_tanh_(use_tanh),
-    Base(name)
+    Base<T>(name)
 {
 }
 
-void Node::drawGraph(cv::Mat& vis, const int sc)
+template <typename T>
+void Node<T>::drawGraph(cv::Mat& vis, const int sc)
 {
   cv::Point pt1 = cv::Point(x_ * sc, y_ * sc);
   
   for (size_t i = 0; i < outputs_.size(); ++i)
   {
-    Node* node = dynamic_cast<Node*>(outputs_[i]);
+    Node<T>* node = dynamic_cast<Node<T>*>(outputs_[i]);
     if (!node) continue;
     cv::Point pt2 = cv::Point(
         node->x_ * sc + rand()%sc/2, 
         node->y_ * sc + rand()%sc/2);
-    const int weight = output_weights_[i]->val_ * 255;
-    const int val = val_ * 255;
+    const int weight = float(output_weights_[i]->val_) * 255;
+    const int val = this->val_ * 255;
     cv::Scalar col = cv::Scalar(255, val, val);
     cv::line(vis, pt1, pt2, col, 1);
   }
 }
 
-Node2d::Node2d(const std::string name, 
+template <typename T>
+Node2d<T>::Node2d(const std::string name, 
     const int x, const int y, const int z, 
     const bool use_var, const bool use_mean, const bool use_tanh) :
-    Node(name, x, y, z, use_var, use_mean, use_tanh)  
+    Node<T>(name, x, y, z, use_var, use_mean, use_tanh)  
 {
 }
-Node3d::Node3d(const std::string name, 
+template <typename T>
+Node3d<T>::Node3d(const std::string name, 
     const int x, const int y, const int z, 
     const bool use_var, const bool use_mean, const bool use_tanh) :
-    Node(name, x, y, z, use_var, use_mean, use_tanh)  
+    Node<T>(name, x, y, z, use_var, use_mean, use_tanh)  
 {
 }
 
-void Node::update()
+template <typename T>
+void Node<T>::update()
 {
   if (inputs_.size() == 0) return;
 
-  val_ = 0;
+  this->val_ = 0;
 
-  float var = 1.0;
-  float mean = 0;
+  T var = 1.0;
+  T mean = 0;
   // normalize
   if (use_mean_ && (inputs_.size() > 1))
   {
-    float sum = 0;
-    float count = 0;
+    T sum = 0;
+    T count = 0;
     for (size_t y = 0; y < inputs_.size(); ++y)
     {
       if (inputs_[y] == NULL) continue;
@@ -144,7 +149,7 @@ void Node::update()
       for (size_t y = 0; y < inputs_.size(); ++y)
       {
         if (inputs_[y] == NULL) continue;
-        const float diff = (inputs_[y]->val_ - mean); 
+        const T diff = (inputs_[y]->val_ - mean); 
         sum += diff * diff;
       }
       // not averaging seems better.
@@ -165,53 +170,55 @@ void Node::update()
       //std::cout << weights_[y][x] << std::endl;
     //std::cout << name_ << " input from " << inputs_[y]->name_ << " " 
     //    << inputs_[y]->val_ << " * " << weights_[y]->val_ << std::endl;
-    val_ += (inputs_[y]->val_ - mean) / var * weights_[y]->val_; 
+    this->val_ += (inputs_[y]->val_ - mean) / var * weights_[y]->val_; 
   }
   
   //std::cout << name_ << " val " <<  val_ << std::endl;
   // TBD apply sigmoid (map to 0.0-1.0) or tanh (-1.0 to 1.0)
   //std::cout << name_ << " val " <<  val_ << " " << tanh(val_) << std::endl;
   if (use_tanh_)
-    val_ = tanh(val_);
+    this->val_ = tanh(this->val_);
 }
 
-void Node2d::setup()
+template <typename T>
+void Node2d<T>::setup()
 {
   if (inputs2_.size() == 0) return;
   if (inputs2_[0].size() == 0) return;
   
-  inputs_.resize(0);
+  this->inputs_.resize(0);
 
-  val_ = 0;
+  this->val_ = 0;
 
   for (size_t y = 0; y < inputs2_.size(); ++y)
   {
     for (size_t x = 0; x < inputs2_[y].size(); ++x)
     {
-      inputs_.push_back(inputs2_[y][x]);
-      weights_.push_back(weights2_[y][x]);
+      this->inputs_.push_back(inputs2_[y][x]);
+      this->weights_.push_back(weights2_[y][x]);
     }
   }
 
   // TBD apply sigmoid or tanh
 }
 
-void Node3d::setup()
+template <typename T>
+void Node3d<T>::setup()
 {
-  if (inputs3_.size() == 0) return;
-  if (inputs3_[0].size() == 0) return;
-  if (inputs3_[0][0].size() == 0) return;
+  if (this->inputs3_.size() == 0) return;
+  if (this->inputs3_[0].size() == 0) return;
+  if (this->inputs3_[0][0].size() == 0) return;
 
-  val_ = 0;
+  this->val_ = T(0);
 
-  for (size_t y = 0; y < inputs3_.size(); ++y)
+  for (size_t y = 0; y < this->inputs3_.size(); ++y)
   {
-    for (size_t x = 0; x < inputs3_[y].size(); ++x)
+    for (size_t x = 0; x < this->inputs3_[y].size(); ++x)
     {
-      for (size_t z = 0; z < inputs3_[y][x].size(); ++z)
+      for (size_t z = 0; z < this->inputs3_[y][x].size(); ++z)
       {
-        inputs_.push_back(inputs3_[y][x][z]);
-        weights_.push_back(weights3_[y][x][z]);
+        this->inputs_.push_back(inputs3_[y][x][z]);
+        this->weights_.push_back(weights3_[y][x][z]);
       }
     }
   }
@@ -220,7 +227,8 @@ void Node3d::setup()
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-Net::Net(cv::Mat& im) :
+template <typename T>
+Net<T>::Net(cv::Mat& im) :
   im_(im)
 {
  
@@ -242,7 +250,7 @@ Net::Net(cv::Mat& im) :
       {
         std::stringstream ss;
         ss << "base_" << i << "_" << y << "_" << x;
-        Weight* weight = new Weight(ss.str());
+        Weight<T>* weight = new Weight<T>(ss.str());
         const float fx = float(x) / float(base_sz);
         const float fy = float(y) / float(base_sz);
         const float wn = sin(fx * M_PI) * sin(fy * M_PI);
@@ -268,7 +276,7 @@ Net::Net(cv::Mat& im) :
     {
       std::stringstream ss;
       ss << "layer1_" << y << "_" << x;
-      Node2d* node = new Node2d(ss.str(), x, y, 0, true, true, true);
+      Node2d<T>* node = new Node2d<T>(ss.str(), x, y, 0, true, true, true);
       node->val_ = float(im.at<uchar>(y,x))/128.0 - 1.0;
       inputs_[y][x] = node;
       nodes_.push_back(node);
@@ -293,7 +301,7 @@ Net::Net(cv::Mat& im) :
         //
         std::stringstream ss;
         ss << "layer2_" << i << "_" << y << "_" << x;
-        Node2d* node = new Node2d(ss.str(), x * div + div/2, y * div + div/2, i, true, true, true);
+        Node2d<T>* node = new Node2d<T>(ss.str(), x * div + div/2, y * div + div/2, i, true, true, true);
         node->inputs2_.resize(bases_[i].size());
         node->weights2_.resize(bases_[i].size());
 
@@ -313,8 +321,8 @@ Net::Net(cv::Mat& im) :
             if ((y2 >= 0) && (y2 < im.rows) &&
                 (x2 >= 0) && (x2 < im.cols))
             {
-              Base* input_weight = bases_[i][k][l];
-              Node* input_node   = dynamic_cast<Node*>( inputs_[y2][x2] );
+              Base<T>* input_weight = bases_[i][k][l];
+              Node<T>* input_node   = dynamic_cast<Node<T>*>( inputs_[y2][x2] );
               
               if ((input_node == NULL))
               {
@@ -358,11 +366,11 @@ Net::Net(cv::Mat& im) :
     {
       std::stringstream ss;
       ss << "layer3_" << y << "_" << x;
-      Node* node = new Node(ss.str(), x, y, 0, true, true, true);
+      Node<T>* node = new Node<T>(ss.str(), x, y, 0, true, true, true);
       layer3_[y][x] = node;
       nodes_.push_back(node);
 
-      Node2d* node_enc = dynamic_cast<Node2d*>(inputs_[y][x]);
+      Node2d<T>* node_enc = dynamic_cast<Node2d<T>*>(inputs_[y][x]);
       //  now loop through outputs of the encoder node - every
       // output of the input node needs to be an input to 
       // this node
@@ -370,8 +378,8 @@ Net::Net(cv::Mat& im) :
       {
         //basis_ind = node->outputs_[i]->z_;
         // TBD replace this with Node::addOutput()
-        Base* input_weight = node_enc->output_weights_[i];
-        Node* input_node = dynamic_cast<Node*>( node_enc->outputs_[i] );
+        Base<T>* input_weight = node_enc->output_weights_[i];
+        Node<T>* input_node = dynamic_cast<Node<T>*>( node_enc->outputs_[i] );
         
         if ((input_node == NULL))
         {
@@ -414,7 +422,7 @@ Net::Net(cv::Mat& im) :
     {
       for (size_t x = 0; x < layer2_[i][y].size(); ++x)
       {
-        Node* input_node   = dynamic_cast<Node*>( layer2_[i][y][x] );
+        Node<T>* input_node   = dynamic_cast<Node<T>*>( layer2_[i][y][x] );
         if ((input_node == NULL))
         {
           std::cerr << "layer 2 bad node " << i << " " << y << " " << x << " " 
@@ -429,7 +437,8 @@ Net::Net(cv::Mat& im) :
 
 }
 
-void Net::update()
+template <typename T>
+void Net<T>::update()
 {
   for (size_t i = 0; i < nodes_.size(); ++i)
   {
@@ -439,7 +448,8 @@ void Net::update()
   }
 }
 
-void layerToMat2D(std::vector< std::vector< Base* > >& layer, cv::Mat& vis) 
+template <typename T>
+void layerToMat2D(std::vector< std::vector< Base<T>* > >& layer, cv::Mat& vis) 
 {
   cv::Mat vis_pre = cv::Mat(cv::Size(layer[0].size(), layer.size()), CV_32FC1);
   //std::cout << "size " << vis_pre.size() << std::endl; 
@@ -471,7 +481,8 @@ void layerToMat2D(std::vector< std::vector< Base* > >& layer, cv::Mat& vis)
 
 }
 
-bool layerToMat(std::vector< Base* >& layer, cv::Mat& vis)
+template <typename T>
+bool layerToMat(std::vector< Base<T>* >& layer, cv::Mat& vis)
 {
   int xmin = 0;
   int xmax = 0;
@@ -481,7 +492,7 @@ bool layerToMat(std::vector< Base* >& layer, cv::Mat& vis)
   // TODO make this a utility function?
   for (size_t i = 0; i < layer.size(); ++i)
   {
-    Node* node = dynamic_cast<Node*>( layer[i] );
+    Node<T>* node = dynamic_cast<Node<T>*>( layer[i] );
     if (node == NULL) 
     {
       continue;
@@ -530,7 +541,7 @@ bool layerToMat(std::vector< Base* >& layer, cv::Mat& vis)
   
   for (size_t i = 0; i < layer.size(); ++i)
   {
-    Node* node = dynamic_cast<Node*>( layer[i] );
+    Node<T>* node = dynamic_cast<Node<T>*>( layer[i] );
 
     size_t x, y;
     if (node == NULL) 
@@ -569,7 +580,8 @@ bool layerToMat(std::vector< Base* >& layer, cv::Mat& vis)
 }
 
 
-void Net::draw()
+template <typename T>
+void Net<T>::draw()
 {
   std::cout << "output " << std::endl;
   {
@@ -665,7 +677,7 @@ int main(int argn, char** argv)
 
   Net* net = new Net(yuvs[0]);
   #endif
-  Net* net = new Net(im);
+  Net<float>* net = new Net<float>(im);
 
   net->update();
   net->draw();
@@ -681,7 +693,7 @@ int main(int argn, char** argv)
       y = (y + net->layer2_[i].size()) % net->layer2_[i].size();
       x = (x + net->layer2_[i][y].size()) % net->layer2_[i][y].size();
       std::cout << "output input " << i << " " << y << " " << x << std::endl;
-      Node* node = dynamic_cast<Node*> (net->layer2_[i][y][x]);
+      Node<float>* node = dynamic_cast<Node<float>*> (net->layer2_[i][y][x]);
       std::cout << "outputs " << node->outputs_.size() << std::endl;
       cv::Mat vis_pre;
       if (layerToMat( node->output_weights_, vis_pre )) 
--- src/nngen/nn_im.hpp
+++ src/nngen/nn_im.hpp
@@ -2,28 +2,32 @@
 #define NN_IM_HPP
 
 // weights are going to be reused so they need to be pointers
+template <typename T>
 class Base
 {
 public:
   Base(std::string name);
   std::string name_;
-  float val_;
+  T val_;
   virtual void update() {}
 };
 
-Base::Base(std::string name) :
+template <typename T>
+Base<T>::Base(std::string name) :
   name_(name)
 {
 
 }
 
-class Weight : public Base
+template <typename T>
+class Weight : public Base<T>
 {
 public:
   Weight(std::string name);
 };
 
-class Node: public Base
+template <typename T>
+class Node: public Base<T>
 {
 public:
   Node(
@@ -47,14 +51,15 @@ public:
   
   virtual void setup() {}
   virtual void update();
-  std::vector< Base* > inputs_;
-  std::vector< Base* > weights_;
+  std::vector< Base<T>* > inputs_;
+  std::vector< Base<T>* > weights_;
   // TODO addOutput() and make these private
-  std::vector< Base* > outputs_;
-  std::vector< Base* > output_weights_;
+  std::vector< Base<T>* > outputs_;
+  std::vector< Base<T>* > output_weights_;
 };
 
-class Node2d : public Node
+template <typename T>
+class Node2d : public Node<T>
 {
 public:
   ///Node2d();
@@ -66,12 +71,13 @@ public:
   virtual void setup();
   // a std vector of Base is not the same as a vector of Node,
   // so have to use the base class
-  std::vector<std::vector< Base* > > inputs2_;
+  std::vector<std::vector< Base<T>* > > inputs2_;
   // TODO maybe this should just be a pointer to a cv::Mat
-  std::vector<std::vector< Base* > > weights2_;
+  std::vector<std::vector< Base<T>* > > weights2_;
 };
 
-class Node3d : public Node
+template <typename T>
+class Node3d : public Node<T>
 {
 public:
   Node3d( 
@@ -82,15 +88,17 @@ public:
   virtual void setup();
   // a std vector of Base is not the same as a vector of Node,
   // so have to use the base class
-  std::vector<std::vector<std::vector< Base* > > > inputs3_;
+  std::vector<std::vector<std::vector< Base<T>* > > > inputs3_;
   // TODO maybe this should just be a pointer to a cv::Mat
-  std::vector<std::vector<std::vector< Base* > > > weights3_;
+  std::vector<std::vector<std::vector< Base<T>* > > > weights3_;
 };
 
-void layerToMat(std::vector< std::vector< Base* > >& layer, cv::Mat& vis, const float sc);
+template <typename T>
+void layerToMat(std::vector< std::vector< Base<T>* > >& layer, cv::Mat& vis, const float sc);
 
 
 
+template <typename T>
 class Net
 {
 public:
@@ -99,16 +107,16 @@ public:
   void update();
   void draw();
 
-  std::vector<std::vector< Base* > > inputs_;
-  std::vector<std::vector<std::vector< Base* > > > layer2_;
-  std::vector<std::vector< Base* > > layer3_;
+  std::vector<std::vector< Base<T>* > > inputs_;
+  std::vector<std::vector<std::vector< Base<T>* > > > layer2_;
+  std::vector<std::vector< Base<T>* > > layer3_;
 
-  std::vector< std::vector< std::vector < Base* > > > bases_;
+  std::vector< std::vector< std::vector < Base<T>* > > > bases_;
  
   cv::Mat im_;
   // all in linear fasion
-  std::vector<Node*> nodes_;
-  std::vector<Base*> weights_;
+  std::vector<Node<T>*> nodes_;
+  std::vector<Base<T>*> weights_;
 };
 
 #endif // NN_IM_HPP
