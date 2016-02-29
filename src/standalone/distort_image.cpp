7ad07605c48e7abb43a2402eb81d757a24b6d737
Lucas Walter <wsacul@gmail.com>
2016-02-29 05:48:34 -0800

    #7 using rqt instead of a bunch of image_views.  Setting d0 to zero messes up the rectification

--- src/standalone/distort_image.cpp
+++ src/standalone/distort_image.cpp
@@ -5,19 +5,21 @@
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
 
-// from http://code.opencv.org/issues/1387
-// TODO(lucasw) need to make a function that only returns the pixel_locations_dst,
-// then the caller can re-use that instead of recomputing it if the intrinsics/dist
-// doesn't change.
-// Also would want to make use of convertMaps to make it even more efficient
-void distort(const cv::Mat& src, cv::Mat& image_dst,
-    const cv::Mat& cameraMatrix, const cv::Mat& distCoeffs)
+// TODO(lucasw) mirror initUndistortRectifyMap as much as makes sense
+// Add m1type parameter
+// And R rectification matrix? 
+// Need to investigate non-floating point version.
+void initDistortMap(const cv::Mat& cameraMatrix, const cv::Mat distCoeffs,
+    const cv::Size size,
+    cv::Mat& map1, cv::Mat& map2)
 {
-  cv::Mat pixel_locations_src = cv::Mat(src.rows * src.cols, 1, CV_32FC2);
+  cv::Mat pixel_locations_src = cv::Mat(size.width * size.height, 1, CV_32FC2);
 
   int ind = 0;
-  for (int i = 0; i < src.size().height; i++) {
-    for (int j = 0; j < src.size().width; j++) {
+  for (int i = 0; i < size.height; i++) {
+    for (int j = 0; j < size.width; j++) {
+      // TODO(lucasw) maybe would want x and y offsets here to make the output
+      // image bigger or smaller than the input?
       pixel_locations_src.at<cv::Point2f>(ind, 0) = cv::Point2f(j,i);
       ++ind;
     }
@@ -33,10 +35,12 @@ void distort(const cv::Mat& src, cv::Mat& image_dst,
 
   // TODO(lucasw) is there a faster way to do this?
   // A matrix operation?
-  cv::Mat pixel_locations_dst = cv::Mat(src.size(), CV_32FC2);
+  // If the x and y were separate matrices it would be possible,
+  // and remap does take separate x and y maps.
+  cv::Mat pixel_locations_dst = cv::Mat(size, CV_32FC2);
   ind = 0;
-  for (int i = 0; i < src.size().height; i++) {
-    for (int j = 0; j < src.size().width; j++) {
+  for (int i = 0; i < size.height; i++) {
+    for (int j = 0; j < size.width; j++) {
       const float x = fractional_locations_dst.at<cv::Point2f>(ind, 0).x * fx + cx;
       const float y = fractional_locations_dst.at<cv::Point2f>(ind, 0).y * fy + cy;
       pixel_locations_dst.at<cv::Point2f>(i,j) = cv::Point2f(x,y);
@@ -46,7 +50,25 @@ void distort(const cv::Mat& src, cv::Mat& image_dst,
     }
   }
 
-  cv::remap(src, image_dst, pixel_locations_dst, cv::Mat(), CV_INTER_LINEAR);
+  map1 = pixel_locations_dst;
+  map2 = cv::Mat();
+}
+
+// adapted from http://code.opencv.org/issues/1387 (which had errors)
+// TODO(lucasw) need to make a function that only returns the pixel_locations_dst,
+// then the caller can re-use that instead of recomputing it if the intrinsics/dist
+// doesn't change.
+// Also would want to make use of convertMaps to make it even more efficient
+void distort(const cv::Mat& src, cv::Mat& image_dst,
+    const cv::Mat& cameraMatrix, const cv::Mat& distCoeffs)
+{
+  cv::Mat map1;
+  cv::Mat map2;
+
+  // TODO(lucasw) will passing in a different size work as expected?
+  // or will it require adjustment of cx/cy?
+  initDistortMap(cameraMatrix, distCoeffs, src.size(), map1, map2);
+  cv::remap(src, image_dst, map1, map2, CV_INTER_LINEAR);
 }
 
 class DistortImage
