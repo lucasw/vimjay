5ffd34a27f183fe60f1e950c82fe9f39651bb2c9
Lucas Walter <wsacul@gmail.com>
2023-05-21 14:59:36 -0700

    changing image.py to publish only when it has received a camera info- TBD update launch files that use it

--- scripts/image.py
+++ scripts/image.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # Lucas Walter
 # GNU GPLv3
-# Load an image from disk and publish it repeatedly
+# Load an image from disk and publish a resized version of it repeatedly as triggered by incoming camera info
 
 import cv2
 import rospy
@@ -10,70 +10,30 @@ from sensor_msgs.msg import CameraInfo, Image
 
 
 class ImagePub:
-    def __init__(self):
-        rospy.init_node('image_publish')
-        name = sys.argv[1]
-        image = cv2.imread(name)
-        # cv2.imshow("im", image)
-        # cv2.waitKey(5)
+    def __init__(self, name):
+        self.image = cv2.imread(name, cv2.IMREAD_COLOR)
+        rospy.loginfo(f"{name} {self.image.shape}")
 
-        hz = rospy.get_param("~rate", 1)
-        frame_id = rospy.get_param("~frame_id", "map")
-        use_image = rospy.get_param("~use_image", True)
-        rate = rospy.Rate(hz)
+        self.image_pub = rospy.Publisher("image", Image, queue_size=1)
 
-        self.ci_in = None
-        self.ci_sub = rospy.Subscriber('camera_info_in', CameraInfo,
+        self.ci_sub = rospy.Subscriber("camera_info_in", CameraInfo,
                                        self.camera_info_callback, queue_size=1)
 
-        if use_image:
-            pub = rospy.Publisher('image', Image, queue_size=1)
-        ci_pub = rospy.Publisher('camera_info', CameraInfo, queue_size=1)
-
-        msg = Image()
-        msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id = frame_id
-        msg.encoding = 'bgr8'
-        msg.height = image.shape[0]
-        msg.width = image.shape[1]
-        msg.step = image.shape[1] * 3
-        msg.data = image.tobytes()
-        if use_image:
-            pub.publish(msg)
-
-        ci = CameraInfo()
-        ci.header = msg.header
-        ci.height = msg.height
-        ci.width = msg.width
-        ci.distortion_model = "plumb_bob"
-        # TODO(lucasw) need a way to set these values- have this node
-        # subscribe to an input CameraInfo?
-        ci.D = [0.0, 0.0, 0.0, 0, 0]
-        ci.K = [500.0, 0.0, msg.width / 2, 0.0, 500.0, msg.height / 2, 0.0, 0.0, 1.0]
-        ci.R = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
-        ci.P = [500.0, 0.0, msg.width / 2, 0.0, 0.0, 500.0, msg.height / 2, 0.0, 0.0, 0.0, 1.0, 0.0]
-        # ci_pub.publish(ci)
-
-        # TODO(lwalter) only run this is hz is positive,
-        # otherwise wait for input trigger message to publish an image
-        while not rospy.is_shutdown():
-            if self.ci_in is not None:
-                ci = self.ci_in
-
-            msg.header.stamp = rospy.Time.now()
-            ci.header = msg.header
-            if use_image:
-                pub.publish(msg)
-            ci_pub.publish(ci)
-
-            if hz <= 0:
-                rospy.sleep()
-            else:
-                rate.sleep()
-
-    def camera_info_callback(self, msg):
-        self.ci_in = msg
+    def camera_info_callback(self, camera_info: CameraInfo):
+        # TODO(lucasw) or use cv bridge or ros_numpy
+        image = Image()
+        image.header = camera_info.header
+        image.encoding = "bgr8"
+        image = cv2.resize(self.image, (camera_info.width, camera_info.height))
+        image.height = image.shape[0]
+        image.width = image.shape[1]
+        image.step = image.shape[1] * 3
+        image.data = image.tobytes()
+        self.image_pub.publish(image)
 
 
-if __name__ == '__main__':
-    image_pub = ImagePub()
+if __name__ == "__main__":
+    rospy.init_node("image_publish")
+    name = sys.argv[1]
+    node = ImagePub(name)
+    rospy.spin()
